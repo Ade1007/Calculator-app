e1ecf0910c081260e02fd5daee1d5a32dae6c361
Ade Harapano <61915515+Ade1007@users.noreply.github.com>
2020-04-15 15:30:39 +0700

    Add files via upload

--- program.cs
+++ program.cs
@@ -1,23 +1,27 @@
 using System;
 
-namespace LatihanInheritanceReal
+namespace ResponsiPemrogramman2910
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person("Ronal", 19);
-            person.GetNameAndAge();
+            Karyawan saya = new Karyawan(01, 91760459, "Ade", 700000);
+            Karyawan kamu = new Karyawan(02, 91824580, "Tata", 500000);
 
-            Teacher teacher = new Teacher("Bambang", 35, 190302, "Guru Bahasa Indonesia");
-            teacher.GetNameAndAge();
+            Console.WriteLine("No \tNik \t \t Nama \t Gaji Bulanan");
+            Console.WriteLine("-------------------------------------");
+            saya.Data();
+            kamu.Data();
 
-            Student student = new Student("Rico", 21, 10506, "Ayanajkt48@gmail.com");
-            student.GetNameAndAge();
+            Console.WriteLine("\n\n Selamat... gaji naik sebesar 10%\n\n");
+
+            Console.WriteLine("No \t Nik \t \t Nama \t Gaji Bulanan");
+            Console.WriteLine("--------------------------------------");
+            saya.Naikgaji();
+            kamu.Naikgaji();
 
             Console.ReadKey();
-
         }
     }
 }
-
